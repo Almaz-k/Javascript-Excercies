0503ac847859ab524912f551b63c592a4e354d84
Almaz-k <74355848+Almaz-k@users.noreply.github.com>
2023-10-05 02:15:32 +0200

    Excercies5DayOfWeek.js

--- Excercise6NumberSequence.js
+++ Excercise6NumberSequence.js
@@ -3,14 +3,13 @@
 // for our case we use while loop cuse the end of the numbers are not defined
 //10 +ve even number : if we divide the number by two and the reminder is zero.
 // how to achive it in JavaScript: use operators % 10%2 ==0
-
 for(let i=2;i<=20;i++){
     if(i%2==0){
         console.log(`number sequence:${i}`);
     }
 }
 
-
+// While loop
 let i=2
 let tenFirstPositiveNumberCounter =0;
 while(tenFirstPositiveNumberCounter<10){
